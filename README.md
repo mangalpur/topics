0b075a9ec94f7fc352133bc05e11020e1bf57c3c
Srinath Mangalpur <Srinath.Mangalpur@rs-components.com>
2019-03-17 13:23:05 +0000

    Initial commit, list of topics relating to year

---
+++ README.md
@@ -0,0 +1,1 @@
+# Topics relating to ks2 maths
---
+++ docs/scripts/topicsApp.js
@@ -0,0 +1,20 @@
+var app = angular.module('myApp',  ['ngTouch']);
+app.controller('myCtrl', function($scope, $http, $timeout) {
+  $http.get("./data/topics.json").then(function (response) {
+      $scope.topics = response.data;
+	  $scope.showActions = false;
+	  $scope.timedOut = false;
+  });
+
+  $timeout( function(){ $scope.timed(); }, 60000*5);  
+   $scope.right= function(topic){
+		topic.year=1;
+		$scope.showActions = !$scope.showActions;
+   };
+   
+   $scope.timed = function(){
+     $scope.timedOut = !$scope.timedOut;
+	// var audio = new Audio('http://localhost:3000/wordsApp/sounds/sound1.mp4');
+     // audio.play();
+   };
+});
